211b3ab634b78af93f4f5533ad2f8f7c7940f781
Viktor <vivchy79@gmail.com>
2022-06-15 22:19:03 +0300

    counter

--- counter.py
+++ counter.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, deque
 
 N_list = ['q', 'q', 'w', 'w', 'd', 'we']
 count_list = Counter(N_list)
@@ -7,3 +7,18 @@ print(count_list)
 N_str = 'шла саша по шоссе и сосала сушку'
 count_str = Counter(N_str)
 print(count_str)
+
+N_str_2 = 'слишком много буков в тексте'
+count_str_2 = Counter(N_str_2)
+
+print(f'объеденить {count_str + count_str_2}')
+print(f'вычесть {count_str - count_str_2}')
+print(f'общее {count_str & count_str_2}')
+
+d = deque('worldofwarcraft')
+print(d.popleft())
+print(d.pop())
+
+car = N_str
+rac = car[::-1] #revers words
+print(rac)
